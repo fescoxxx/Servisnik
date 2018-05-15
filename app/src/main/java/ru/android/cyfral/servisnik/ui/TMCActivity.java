0507bfe9b5be25378de257323457b189993f6389
Tsarev Evgeniy <joeklop35@mail.ru>
2018-05-15 16:40:13 +0700

    Правки вида

---
+++ app/src/main/java/ru/android/cyfral/servisnik/ui/TMCActivity.java
@@ -0,0 +1,52 @@
+package ru.android.cyfral.servisnik.ui;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+
+import java.util.List;
+
+import ru.android.cyfral.servisnik.R;
+import ru.android.cyfral.servisnik.model.OrderCard.OrderCard;
+import ru.android.cyfral.servisnik.model.tmc.TmcAdapter;
+
+public class TMCActivity extends AppCompatActivity {
+
+    private List<String> listTmc;
+    private TmcAdapter tmcAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tmc);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        ActionBar actionBar =getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        setTitle("Плановые затраты ТМЦ");
+        Intent intent = getIntent();
+        OrderCard orderCard = (OrderCard) intent.getExtras().getSerializable("ordercard");
+        listTmc = orderCard.getData().getTmas();
+         tmcAdapter = new TmcAdapter(this, listTmc);
+        // настраиваем список
+         ListView lv_tmc = (ListView) findViewById(R.id.lv_tmc);
+         lv_tmc.setAdapter(tmcAdapter);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
