a5613f08487d0d661a6883b0558cd8a1cda10cd3
Tsarev Evgeniy <joeklop35@mail.ru>
2018-05-16 19:50:12 +0700

    4 экрана

---
+++ app/src/main/java/ru/android/cyfral/servisnik/model/safeHome/SafeHomeAdapter.java
@@ -0,0 +1,113 @@
+package ru.android.cyfral.servisnik.model.safeHome;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+
+import ru.android.cyfral.servisnik.R;
+import ru.android.cyfral.servisnik.model.OrderCard.InstalledEquipments;
+import ru.android.cyfral.servisnik.model.OrderCard.Items;
+
+public class SafeHomeAdapter extends BaseAdapter {
+
+    private static final int TYPE_ITEM = 0;
+    private static final int TYPE_SEPARATOR = 1;
+    Context ctx;
+    LayoutInflater lInflater;
+    List<Items> objects = new ArrayList<Items>();;
+
+    private TreeSet<Integer> sectionHeader = new TreeSet<Integer>();
+    public SafeHomeAdapter(Context context) {
+        ctx = context;
+        lInflater = (LayoutInflater) ctx
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void addItem(final Items item) {
+        objects.add(item);
+        notifyDataSetChanged();
+    }
+    public void addSectionHeaderItem(final Items item) {
+        objects.add(item);
+        sectionHeader.add(objects.size() - 1);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return sectionHeader.contains(position) ? TYPE_SEPARATOR : TYPE_ITEM;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public int getCount() {
+        return objects.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return objects.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        int rowType = getItemViewType(position);
+
+        if (convertView == null) {
+            switch (rowType) {
+                case TYPE_ITEM:
+                    final Items item = getItems(position);
+                    convertView = lInflater.inflate(R.layout.row_item_safe_house, null);
+                    ((TextView) convertView.findViewById(R.id.safe_house_title)).setText(item.getTitle());
+                    ((TextView) convertView.findViewById(R.id.safe_house_body)).setText(item.getBody());
+                    break;
+                case TYPE_SEPARATOR:
+                    final Items itemHedaer = getItems(position);
+                    convertView = lInflater.inflate(R.layout.header_row_item_safe_house, null);
+                    ((TextView) convertView.findViewById(R.id.header_safe_house)).setText(itemHedaer.getTitle());
+                    break;
+            }
+        }
+
+
+        return convertView;
+
+        // View view = convertView;
+
+
+
+      /*  if (view == null) {
+            view = lInflater.inflate(R.layout.row_item_safe_house, parent, false);
+        }
+        final Items item = getItems(position);
+        ((TextView) view.findViewById(R.id.safe_house_title)).setText(item.getTitle());
+        ((TextView) view.findViewById(R.id.safe_house_body)).setText(item.getBody());*/
+       // return null;
+    }
+
+
+    public static class ViewHolder {
+        public TextView textView;
+    }
+    // по позиции
+    Items getItems(int position) {
+        return ((Items) getItem(position));
+    }
+}
--- app/src/main/java/ru/android/cyfral/servisnik/ui/SafeHouseActivity.java
+++ app/src/main/java/ru/android/cyfral/servisnik/ui/SafeHouseActivity.java
@@ -1,15 +1,94 @@
 package ru.android.cyfral.servisnik.ui;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
 
 import ru.android.cyfral.servisnik.R;
+import ru.android.cyfral.servisnik.model.OrderCard.Items;
+import ru.android.cyfral.servisnik.model.OrderCard.OrderCard;
+import ru.android.cyfral.servisnik.model.OrderCard.SafeHome;
+import ru.android.cyfral.servisnik.model.equipments.EquipmentsAdapter;
+import ru.android.cyfral.servisnik.model.safeHome.SafeHomeAdapter;
 
 public class SafeHouseActivity extends AppCompatActivity {
+    private List<SafeHome> listSafeHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_safe_house); //
+        setContentView(R.layout.activity_safe_house);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        ActionBar actionBar =getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        setTitle("Безопасный дом");
+        Intent intent = getIntent();
+        OrderCard orderCard = (OrderCard) intent.getExtras().getSerializable("ordercard");
+        listSafeHome = orderCard.getData().getSafeHome();
+
+        SafeHomeAdapter mAdapter = new SafeHomeAdapter(this);
+        ListView lv_safe_house = (ListView) findViewById(R.id.lv_safe_house);
+        for (int i=0; i<listSafeHome.size(); i++) {
+            Items itemTitle = new Items();
+            itemTitle.setTitle(listSafeHome.get(i).getTitle());
+            mAdapter.addSectionHeaderItem(itemTitle);
+            List<Items> listItems = listSafeHome.get(i).getItems();
+            if (!listItems.isEmpty()) {
+                for (int x=0; x<listItems.size(); x++) {
+                        Items item = new Items();
+                        item.setTitle(listItems.get(x).getTitle());
+                        item.setBody(listItems.get(x).getBody());
+                        mAdapter.addItem(item);
+                 }
+            }
+
+        }
+        lv_safe_house.setAdapter(mAdapter);
+   /*     for (int i=0; i<listSafeHome.size(); i++) {
+           // настраиваем список
+        try {
+            ListView lv_safe_house = (ListView) findViewById(R.id.lv_safe_house);
+            List<Items> item = orderCard.getData().getSafeHome().get(i).getItems();
+            SafeHomeAdapter safeHomeAdapter = new SafeHomeAdapter(this, item);
+            // настраиваем список
+
+            View header = getLayoutInflater().inflate(R.layout.header_row_item_safe_house, null);
+            TextView hdr = (TextView) header.findViewById(R.id.header_safe_house);
+            lv_safe_house = (ListView) findViewById(R.id.lv_safe_house);
+            hdr.setText(orderCard.getData().getSafeHome().get(i).getTitle());
+            lv_safe_house.addHeaderView(header);
+            lv_safe_house.setAdapter(safeHomeAdapter);
+        } catch (NullPointerException ex) {}*/
+
+
     }
+
+ /*          ListView lv_safe_house = (ListView) findViewById(R.id.lv_safe_house);
+           View header = getLayoutInflater().inflate(R.layout.header_row_item_safe_house, null);
+           TextView hdr = (TextView) header.findViewById(R.id.header_safe_house);
+           lv_safe_house.addHeaderView(header);
+           safeHomeAdapter = new SafeHomeAdapter(this, listSafeHome.get(0).getItems());*/
+    //   }
+         @Override
+         public boolean onOptionsItemSelected(MenuItem item) {
+             switch (item.getItemId()) {
+                 case android.R.id.home:
+                     this.finish();
+                     return true;
+                 default:
+                     return super.onOptionsItemSelected(item);
+             }
+         }
+
 }
