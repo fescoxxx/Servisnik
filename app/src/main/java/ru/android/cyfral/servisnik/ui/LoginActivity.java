c76056b3fcb688a8b9b01f54eec99710237f7545
Tsarev Evgeniy <joeklop35@mail.ru>
2018-05-22 20:05:51 +0700

    черточка зн

--- app/src/main/java/ru/android/cyfral/servisnik/ui/LoginActivity.java
+++ app/src/main/java/ru/android/cyfral/servisnik/ui/LoginActivity.java
@@ -73,19 +73,19 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private void login() {
-        Call<Token> call = tokenClient.login("password",
+/*        Call<Token> call = tokenClient.login("password",
                 "mpservisnik",
                 "secret",
                 "s.sidorov",
                 "!Qwerty7",
-                "");
+                "");*/
 
-/*        Call<Token> call = tokenClient.login("password",
+        Call<Token> call = tokenClient.login("password",
                 "mpservisnik",
                 "secret",
                 username.getText().toString(),
                 password.getText().toString(),
-        "");*/
+        "");
 
 
 
