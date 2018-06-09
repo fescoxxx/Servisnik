280162f85c0a8b29edcca1fba65e35c1f89ad067
Tsarev Evgeniy <joeklop35@mail.ru>
2018-06-09 09:35:55 +0700

    карта

---
+++ app/src/main/java/ru/android/cyfral/servisnik/model/listwork/Data.java
@@ -0,0 +1,57 @@
+package ru.android.cyfral.servisnik.model.listwork;
+
+public class Data {
+    private String repairRequestDeadline;
+
+    private String longitude;
+
+    private String latitude;
+
+    private String houseId;
+
+    public String getRepairRequestDeadline ()
+    {
+        return repairRequestDeadline;
+    }
+
+    public void setRepairRequestDeadline (String repairRequestDeadline)
+    {
+        this.repairRequestDeadline = repairRequestDeadline;
+    }
+
+    public String getLongitude ()
+    {
+        return longitude;
+    }
+
+    public void setLongitude (String longitude)
+    {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude ()
+    {
+        return latitude;
+    }
+
+    public void setLatitude (String latitude)
+    {
+        this.latitude = latitude;
+    }
+
+    public String getHouseId ()
+    {
+        return houseId;
+    }
+
+    public void setHouseId (String houseId)
+    {
+        this.houseId = houseId;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [repairRequestDeadline = "+repairRequestDeadline+", longitude = "+longitude+", latitude = "+latitude+", houseId = "+houseId+"]";
+    }
+}
