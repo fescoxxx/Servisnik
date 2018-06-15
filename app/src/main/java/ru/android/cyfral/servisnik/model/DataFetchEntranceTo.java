4b74aff62ceebad2b2585163bd059912387a568e
fescoxxx <darombeer@gmail.com>
2018-06-15 13:21:48 +0700

    Подъезды на ТО

---
+++ app/src/main/java/ru/android/cyfral/servisnik/model/DataFetchEntranceTo.java
@@ -0,0 +1,7 @@
+package ru.android.cyfral.servisnik.model;
+
+import ru.android.cyfral.servisnik.model.entranceto.EntranceTo;
+
+public interface DataFetchEntranceTo {
+    void onDeliverData(EntranceTo entranceTo);
+}
