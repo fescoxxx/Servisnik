2dd0abcaa908779680217b0c7b805442b859d0c6
fescoxxx <darombeer@gmail.com>
2018-06-17 11:28:18 +0700

    Подъезды на ТО

--- app/src/main/java/ru/android/cyfral/servisnik/ui/LoginActivity.java
+++ app/src/main/java/ru/android/cyfral/servisnik/ui/LoginActivity.java
@@ -1,18 +1,25 @@
 package ru.android.cyfral.servisnik.ui;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -35,6 +42,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private ProgressDialog mDialog; //анимация загрузки
     private TextView username;
     private TextView password;
+    private ImageView image_logo; //лого
+    private TextView login_text_error;
+    private TextView psw_text_error;
 
     View.OnClickListener mOnClickListener;
 
@@ -49,7 +59,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         try{
             getSupportActionBar().hide();
-        } catch (NullPointerException ex) {}
+        } catch (NullPointerException ex) {
+
+        }
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         setContentView(R.layout.activity_main);
         //скрыть snackbar
@@ -64,6 +76,29 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         username = findViewById(R.id.text_username);
         password = findViewById(R.id.text_password);
+        image_logo  = (ImageView) findViewById(R.id.image_logo);
+        login_text_error =(TextView) findViewById(R.id.login_text_error);
+        psw_text_error = (TextView) findViewById(R.id.psw_text_error);
+
+        login_text_error.setVisibility(View.INVISIBLE);
+        psw_text_error.setVisibility(View.INVISIBLE);
+
+        final ConstraintLayout rootView = (ConstraintLayout) findViewById(R.id.constants_root);
+        rootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                Rect r = new Rect();
+                rootView.getWindowVisibleDisplayFrame(r);
+                int screenHeight = rootView.getRootView().getHeight();
+                int keypadHeight = screenHeight - r.bottom;
+                if (keypadHeight > screenHeight * 0.15) {
+                    image_logo.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    image_logo.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     public static String getFormatDate(Date date) {
@@ -142,12 +177,37 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }/**/
 
 
+    public void validator() {
+        boolean error = false;
+        if (username.getText().toString().length() == 0) {
+            login_text_error.setVisibility(View.VISIBLE);
+            username.getBackground().mutate().setColorFilter(getResources().getColor(R.color.dark_red), PorterDuff.Mode.SRC_ATOP);
+            error = true;
+        } else {
+            login_text_error.setVisibility(View.INVISIBLE);
+            username.getBackground().mutate().setColorFilter(getResources().getColor(R.color.dark), PorterDuff.Mode.SRC_ATOP);
+            error = false;
+        }
+
+        if (password.getText().toString().length() == 0) {
+            psw_text_error.setVisibility(View.VISIBLE);
+            password.getBackground().mutate().setColorFilter(getResources().getColor(R.color.dark_red), PorterDuff.Mode.SRC_ATOP);
+            error = true;
+        } else {
+            psw_text_error.setVisibility(View.INVISIBLE);
+            password.getBackground().mutate().setColorFilter(getResources().getColor(R.color.dark), PorterDuff.Mode.SRC_ATOP);
+            error = false;
+        }
+
+        if (!error) {login();}
+
+    }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_login:
-                login();
+                validator();
                 break;
             default:
                 // TODO Auto-generated method stub
